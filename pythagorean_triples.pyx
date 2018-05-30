b807a3b3e07bc9873c9ba6846210cf6484197a0a
Vladislav Stognienko <stognienkovv@gmail.com>
2018-05-30 12:44:06 +0300

    cython hard

--- pythagorean_triples.pyx
+++ pythagorean_triples.pyx
@@ -2,7 +2,11 @@ import time
 
 
 def count(limit):
-    result = 0
+    cdef int result = 0
+    cdef int a = 0
+    cdef int b = 0
+    cdef int c = 0
+
     for a in range(1, limit + 1):
         for b in range(a + 1, limit + 1):
             for c in range(b + 1, limit + 1):
